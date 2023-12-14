55d2bda9237ff1f4a83ce7928b98ac33c7136f1c
AKD <contact.mousom@gmail.com>
2023-12-15 00:04:21 +0600

    Day 5 work done

---
+++ DAY_05/DAY_05.ts
@@ -0,0 +1,17 @@
+/**
+ * Today i learned about how can i declear type.
+ * We have to create an variable without value
+ * Now we have to attach the types in the variables
+ */
+
+let person:string; //You have to declear the type like this
+
+person = 'AKD';
+
+console.log(person);
+
+person = 22; //Type 'number' is not assignable to type 'string'.
+
+console.log(person);
+
+
