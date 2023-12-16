0c6c3ed1f7437e8fb4a4edc3762dfd0737bf52d7
AKD <contact.mousom@gmail.com>
2023-12-17 03:26:23 +0600

    Some folder name change and day 6 task done

---
+++ DAY_06/DAY_06.ts
@@ -0,0 +1,12 @@
+// To specify the type of an array like [1, 2, 3], you can use the syntax number[]
+// You may also see this written as Array<number>, which means the same thing. We’ll learn more about the syntax T<U> when we cover generics.
+
+const arr:Number[] = [1,2,3,4,5]
+
+arr.push(123,321,221,22,)
+
+const arr2:Array<Number>=[1,2,3,4,5]
+
+arr2.push(123,321,221,22,)
+
+
