3df0e0753afb337e352c6298403f47453fa5944d
AKD <contact.mousom@gmail.com>
2023-12-18 01:24:00 +0600

    Union type implementation done

---
+++ DAY_07/DAY_07.js
@@ -0,0 +1,5 @@
+// Union Type
+function printId(id) {
+    console.log("Your ID is: " + id);
+}
+printId(2);
---
+++ DAY_07/DAY_07.ts
@@ -0,0 +1,9 @@
+// Union Type
+
+function printId(id: number | string) {
+    console.log("Your ID is: " + id);
+  }
+
+  printId(2)
+
+
